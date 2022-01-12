67c38d611afb1fd27048479af5a228d35a7e80c4
Conner Hubbell <hubb2447@bears.unco.edu>
2022-01-12 12:02:43 -0700

    yuh

---
+++ Output 3/Output 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Output_3
+{
+    class Program
+    {
+        public static int calcSum(int[] arr) {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        public static float calcAverage(int[] arr){
+            float average = 0.0F;
+            average = (float)calcSum(arr) / arr.Length;
+            return average;
+        }
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            int[] arr = { 1, 5, 6, 3, 12, 11, 10, 56, 342, 7 };       
+            Console.WriteLine("Sum: " + Program.calcSum(arr));
+            Console.WriteLine("Average: " + Program.calcAverage(arr));
+        }
+    }
+}
