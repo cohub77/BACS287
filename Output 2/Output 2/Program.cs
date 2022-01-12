53f42d76599e81f737800dd9bedaffda04d3b96e
Conner Hubbell <hubb2447@bears.unco.edu>
2022-01-12 11:42:18 -0700

    ayyy

---
+++ Output 2/Output 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Output_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 25; i >= 0; i--)
+                Console.WriteLine(i);
+        }
+    }
+}
